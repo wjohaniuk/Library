15bd21e210266d29d0b9f90834bcf1777bd823cb
wjohaniuk <32334253+wjohaniuk@users.noreply.github.com>
2020-01-22 12:56:24 +0100

    zad Tablice

---
+++ src/pl/employee/Employee.java
@@ -0,0 +1,37 @@
+package pl.employee;
+
+public class Employee {
+private String firstName;
+private String lastName;
+private int salary;
+
+    public Employee(String firstName, String lastName, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
