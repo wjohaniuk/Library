7379b29cbd15c9cc5c5c9f875861c6c90b70328f
wjohaniuk <32334253+wjohaniuk@users.noreply.github.com>
2020-01-22 14:05:28 +0100

    lib 0.7 tabs added

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -4,19 +4,21 @@ import pl.javastart.library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.7";
 
-        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
+        Book[] books = new Book[1000];
+
+        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "987456123");
-        Book book2 = new Book("Java dla początkujących", "Joshua Bloch", 2013,
+        books[1] = new Book("Java dla początkujących", "Joshua Bloch", 2013,
                 496, "Helion", "32156489");
-        Book book3 = new Book("Sun Certified Programmer for Java", "Bert Bates", 2008,
+        books[2] = new Book("Sun Certified Programmer for Java", "Bert Bates", 2008,
                 896, "McGraw-Hill");
 
         System.out.println(appName);
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
 
     }
 }
